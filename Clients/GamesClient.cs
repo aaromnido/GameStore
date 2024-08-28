d77baf7859bc9f7310b20a9546c751e98ef7e147
Fernando <fernando@inspiration-q.com>
2024-08-28 10:22:15 +0200

    Initial Commit

---
+++ Clients/GamesClient.cs
@@ -0,0 +1,53 @@
+﻿using GameStore.Models;
+
+namespace GameStore.Clients;
+
+
+public class GamesClient
+{
+    private readonly List<GameSummary> games =
+    [
+        new(){
+            Id = 1,
+            Name = "Street Fighter II",
+            Genre = "Fighting",
+            Price = 19.99M,
+            ReleaseDate = new DateOnly(1991, 7, 15)
+        },
+        new(){
+            Id = 2,
+            Name = "Final Fantasy",
+            Genre = "Roleplaying",
+            Price = 59.99M,
+            ReleaseDate = new DateOnly(2010, 9, 30)
+        },
+        new(){
+            Id = 3,
+            Name = "FIFA 23",
+            Genre = "Sports",
+            Price = 69.99M,
+            ReleaseDate = new DateOnly(1922, 9, 27)
+        },
+    ];
+
+    private readonly Genre[] genres = new GenresClient().GetGenres();
+
+    public GameSummary[] GetGames() => games.ToArray();
+
+    public void AddGame(GameDetails game)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId);
+        var genre = genres.Single(genre => genre.Id == int.Parse(game.GenreId));
+
+        var GameSummary = new GameSummary
+        {
+            Id = games.Count + 1,
+            Name = game.Name,
+            Genre = genre.Name,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+        };
+
+        games.Add(GameSummary);
+    }
+}
---
+++ Clients/GenresClient.cs
@@ -0,0 +1,33 @@
+﻿using GameStore.Models;
+
+namespace GameStore.Clients;
+
+public class GenresClient
+{
+    private readonly Genre[] genres =
+    [
+        new (){
+            Id = 1,
+            Name = "Fighting"
+        },
+        new (){
+            Id = 2,
+            Name = "Roleplaying"
+        },
+        new (){
+            Id = 3,
+            Name = "Sports"
+        },
+        new (){
+            Id = 4,
+            Name = "Racing"
+        },
+        new (){
+            Id = 5,
+            Name = "Kids and Family"
+        },
+
+    ];
+
+    public Genre[] GetGenres() => genres;
+}
